3e60abae0dcbc7df08d6032669a6242d24324ba1
mamane19 <b.moussaama@alustudent.com>
2022-02-20 18:20:04 +0200

    feat(aut): adding supertokens module

--- src/auth/auth.middleware.ts
+++ src/auth/auth.middleware.ts
@@ -1,7 +1,12 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
+import { middleware } from 'supertokens-node/framework/express/';
 
 @Injectable()
 export class AuthMiddleware implements NestMiddleware {
+  superTokensMiddleware: any;
+  constructor() {
+    this.superTokensMiddleware = middleware();
+  }
   use(req: any, res: any, next: () => void) {
     next();
   }
