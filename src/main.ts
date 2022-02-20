5fed2c5a442392d9c9abb3417e0b3bcde3e28d2e
mamane19 <b.moussaama@alustudent.com>
2022-02-20 18:24:58 +0200

    feat(aut): adding supertokens module

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,17 @@ import supertokens from 'supertokens-node';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*',
+    allowedHeaders: [
+      'Content-Type',
+      'Authorization',
+      'Accept',
+      ...supertokens.getAllCORSHeaders(),
+    ],
+    credentials: true,
+  });
   await app.listen(3000);
 }
 bootstrap();
