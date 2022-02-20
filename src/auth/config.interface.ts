3e2c2ff4e055416bc769b4226fa2d9e5c631c145
mamane19 <b.moussaama@alustudent.com>
2022-02-20 17:55:04 +0200

    feat(aut): adding supertokens module

---
+++ src/auth/config.interface.ts
@@ -0,0 +1,11 @@
+import { AppInfo } from 'supertokens-node/lib/build/types';
+
+export const configInjectionToken = 'config';
+
+export type AuthModuleConfig = {
+  appInfo: AppInfo;
+  connectionURI: string;
+  apiKey?: string;
+};
+
+
