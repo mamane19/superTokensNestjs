d3d07ff09c3f744a83e0ae9f2f08a9181e543669
mamane19 <b.moussaama@alustudent.com>
2022-02-20 18:23:14 +0200

    feat(aut): adding supertokens module

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import supertokens from 'supertokens-node';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
